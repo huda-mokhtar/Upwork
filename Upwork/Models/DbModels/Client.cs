45b2c2e5ada17219e286f45ee824367f8e66a868
HadeerMagdy22 <77938511+HadeerMagdy22@users.noreply.github.com>
2021-06-12 13:51:25 +0200

    Create Client.cs
    Client Model added

---
+++ Upwork/Models/DbModels/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Upwork.Models.DbModels
+{
+    public class Client
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [ForeignKey("User")]
+        public string FreelancerId { get; set; }
+
+
+    }
+}
