a09100d9f12bc41e75ed2341c7dd80a9366cc30a
TasnimSalah <tasnemsalah923@gmail.com>
2021-06-13 12:19:32 +0200

    update

---
+++ Upwork/Controllers/ClientController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Upwork.Data;
+
+namespace Upwork.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PostJob()
+        {
+            return View();
+        }
+        public IActionResult Steps()
+        {
+            return View();
+        }
+    }
+}
